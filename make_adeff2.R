077c3a40795bb59a0c970b49ab8473f961fecc43
kazemia <kazemiamirhos@gmail.com>
2024-01-18 12:49:55 +0100

    Initial push

---
+++ make_adeff2.R
@@ -0,0 +1,155 @@
+######################
+# This program takes the raw data and make the adeff.rds dataset. 
+# adeff is short for Ananlysis Dataset Efficacy. 
+############################################
+
+
+library(tidyverse)
+
+
+TLevels <- c("Inf", "Ada", "Cer", "Eta", "Gol")
+Z <- list("2010" = c("Inf", "Gol", "Cer", "Eta", "Ada"),
+          "2011" = c("Eta", "Inf", "Cer", "Gol", "Ada"),
+          "2012" = c("Inf", "Eta", "Cer", "Gol", "Ada"),
+          "2013" = c("Cer", "Inf", "Gol", "Eta", "Ada"),
+          "2014" = c("Inf", "Cer", "Gol", "Ada", "Eta"),
+          "2015" = c("Inf", "Cer", "Gol", "Eta", "Ada"),
+          "2016" = c("Inf", "Cer", "Eta", "Gol", "Ada"),
+          "2017" = c("Inf", "Eta", "Gol", "Cer", "Ada"),
+          "2018" = c("Inf", "Eta", "Cer", "Ada", "Gol"),
+          "2019" = c("Ada", "Inf", "Eta", "Cer", "Gol") 
+)
+# 
+# Z_ <- as_tibble(Z) %>% 
+#   mutate(place = 1:n()) %>% 
+#   pivot_longer(cols = c(1:10), names_to = "Year", values_to = "trt") %>% 
+#   arrange(Year, place) %>% 
+#   group_by(Year) %>% 
+#   nest() %>% 
+#   rename(tender = data) %>% 
+#   mutate(Year = as.integer(Year))
+
+# Ready the dataset
+load("C:/Users/kazem/Documents/Data/myData.rdata")
+data <- as.data.frame(my_data)
+remove(my_data)
+
+
+data <- data %>% filter(!is.na(bldt.x))
+data$Z_value <- NA
+data$m <- NA
+NDPC_mid_date <- as.Date(c())
+for(y in 2010:2013){
+  StartDate <- as.Date(paste0(y, "-01-31"), "%Y-%m-%d")
+  EndDate <- as.Date(paste0(y+1, "-02-01"), "%Y-%m-%d")
+  NDPC_mid_date <- 
+    c(NDPC_mid_date, 
+      StartDate + floor(as.numeric(
+        difftime(EndDate, StartDate, units = "days"))/2))
+  data$Z_value[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- y-2009
+  data$m[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- 
+    as.numeric(difftime(data$bldt.x[
+      (data$bldt.x > StartDate) & (data$bldt.x < EndDate)], 
+      StartDate, units = "days"))
+}
+y <- 2014
+StartDate <- as.Date(paste0(y, "-01-31"), "%Y-%m-%d")
+EndDate <- as.Date(paste0(y+1, "-03-01"), "%Y-%m-%d")
+NDPC_mid_date <- 
+  c(NDPC_mid_date, 
+    StartDate + floor(as.numeric(
+      difftime(EndDate, StartDate, units = "days"))/2))
+data$Z_value[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- y-2009
+data$m[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- 
+  as.numeric(difftime(data$bldt.x[
+    (data$bldt.x > StartDate) & (data$bldt.x < EndDate)],
+    StartDate, units = "days"))
+y <- 2015
+StartDate <- as.Date(paste0(y, "-02-28"), "%Y-%m-%d")
+EndDate <- as.Date(paste0(y+1, "-03-01"), "%Y-%m-%d")
+NDPC_mid_date <- 
+  c(NDPC_mid_date, 
+    StartDate + floor(as.numeric(
+      difftime(EndDate, StartDate, units = "days"))/2))
+data$Z_value[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- y-2009
+data$m[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- 
+  as.numeric(difftime(data$bldt.x[
+    (data$bldt.x > StartDate) & (data$bldt.x < EndDate)],
+    StartDate, units = "days"))
+y <- 2016
+StartDate <- as.Date(paste0(y, "-02-29"), "%Y-%m-%d")
+EndDate <- as.Date(paste0(y+1, "-03-01"), "%Y-%m-%d")
+NDPC_mid_date <- 
+  c(NDPC_mid_date, 
+    StartDate + floor(as.numeric(
+      difftime(EndDate, StartDate, units = "days"))/2))
+data$Z_value[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- y-2009
+data$m[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- 
+  as.numeric(difftime(data$bldt.x[
+    (data$bldt.x > StartDate) & (data$bldt.x < EndDate)],
+    StartDate, units = "days"))
+y <- 2017
+StartDate <- as.Date(paste0(y, "-02-28"), "%Y-%m-%d")
+EndDate <- as.Date(paste0(y+1, "-02-01"), "%Y-%m-%d")
+NDPC_mid_date <- 
+  c(NDPC_mid_date, 
+    StartDate + floor(as.numeric(
+      difftime(EndDate, StartDate, units = "days"))/2))
+data$Z_value[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- y-2009
+data$m[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- 
+  as.numeric(difftime(data$bldt.x[
+    (data$bldt.x > StartDate) & (data$bldt.x < EndDate)],
+    StartDate, units = "days"))
+for(y in 2018:2019){
+  StartDate <- as.Date(paste0(y, "-01-31"), "%Y-%m-%d")
+  EndDate <- as.Date(paste0(y+1, "-02-01"), "%Y-%m-%d")
+  NDPC_mid_date <- 
+    c(NDPC_mid_date, 
+      StartDate + floor(as.numeric(
+        difftime(EndDate, StartDate, units = "days"))/2))
+  data$Z_value[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- y-2009
+  data$m[(data$bldt.x > StartDate) & (data$bldt.x < EndDate)] <- 
+    as.numeric(difftime(data$bldt.x[
+      (data$bldt.x > StartDate) & (data$bldt.x < EndDate)],
+      StartDate, units = "days"))
+}
+
+data <- data %>% filter(!is.na(Z_value))
+data$das28crprem <- as.numeric(data$das28crprem)
+data$Year = as.integer(2009 + data$Z_value)
+data$cal_months <- as.numeric(difftime(data$bldt.x,
+                                       as.Date("2010-01-31", "%Y-%m-%d"), 
+                                       units = "days"))/30.417
+
+
+
+adeff <- data %>%
+  mutate(trtgrp = factor(trtgrp.x, level = TLevels),
+         das28crp_BL = if_else(is.na(das28crp_BL), das28_BL, das28crp_BL)) %>%
+  rename(subjid = patid.x, bldt = bldt.x) %>%
+  select(-trtgrp.x)
+
+temp <- adeff %>% select(tcid, trtgrp) %>% unique()
+
+adeff <- adeff  %>% select(-subjid, -bldt, -trtgrp) %>% 
+  group_by(tcid) %>% 
+  summarise_if(is.numeric,median, na.rm = TRUE) %>%
+  as.data.frame() %>%
+  mice::mice(m = 1) %>%
+  mice::complete() %>%
+  merge(temp)
+
+save(adeff, file = "adeff2.Rdata")
+
+#adanon <- adeff %>%
+#  mutate(month = floor(cal_months)+1) %>% 
+#  select(trtgrp, das28crp_bl, das28crprem, Z_value, month) 
+
+
+#library(haven)
+#write_dta(adeff, "data/adeff.dta")
+#readr::write_rds(adeff, "data/adeff.rds")
+#write_csv(adanon, "data/adanon.csv")
+
+
+
